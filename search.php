5137a181af1ae8cd6b566a870bf09048d126821c
Lumartist <englandsking@gmail.com>
2014-11-05 16:14:35 +0100

    Search Result

--- search.php
+++ search.php
@@ -1,26 +1,40 @@
 <?php get_header(); ?>
- 
-   <div id="main">
-      <?php if (have_posts()) : ?>
-         <p class="info">Deine Suchergebnisse f&uuml;r <strong><?php echo $s ?></strong></p>
- 
-         <?php while (have_posts()) : the_post(); ?>
-            <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-            <div class="entry">123
-               <?php the_content(); ?>
+
+<div class="ink-grid">
+    <div class="column-group">    
+        <div class="xlarge-100 large-100 medium-100 small-100 tiny-100 main_bg">
+            <div class="column-group">
+                <!-- Main Content -->
+                <div class="xlarge-66 large-66 medium-66 small-100 tiny-100 main_content">
+                    
+                	<?php if (have_posts()) : ?>
+                         <p class="info">Deine Suchergebnisse f&uuml;r <strong><?php echo $s ?></strong></p>
+                 
+                         <?php while (have_posts()) : the_post(); ?>
+                            <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+                            <div class="entry">123
+                               <?php the_content(); ?>
+                            </div>
+                         <?php endwhile; ?>
+                 
+                         <p align="center"><?php next_posts_link('&laquo; &Auml;ltere Eintr&auml;ge') ?> | <?php previous_posts_link('Neuere Eintr&auml;ge &raquo;') ?></p>
+                 
+                      <?php else : ?>
+                         <h2>Leider nichts gefunden</h2>
+                 
+                      <?php endif; ?>                      
+                    
+                </div>
+                <!-- /Main Content -->
+                <!-- Sidebar -->
+                <div class="xlarge-33 large-33 medium-33 small-100 tiny-100">                     
+                    <?php include( TEMPLATEPATH . '/template/sidebar_news.php' ); ?>
+                    <?php include( TEMPLATEPATH . '/template/sidebar_topnews.php' ); ?>
+                </div>
+                <!-- /Sidebar -->
             </div>
-         <?php endwhile; ?>
- 
-         <p align="center"><?php next_posts_link('&laquo; &Auml;ltere Eintr&auml;ge') ?> | <?php previous_posts_link('Neuere Eintr&auml;ge &raquo;') ?></p>
- 
-      <?php else : ?>
-         <h2>Leider nichts gefunden</h2>
- 
-      <?php endif; ?>
-   </div><!-- main -->
- 
-   <div id="sidebar">
-      <?php get_sidebar(); ?>
-   </div><!-- sidebar --> 
- 
+        </div>
+    </div>
+</div>
+
 <?php get_footer(); ?>
